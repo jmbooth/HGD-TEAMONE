23943f838f8f4f617798c3020e0f35707f132861
mdkempf <mdkempf@portal.lab.huskygames.com>
2016-01-31 13:53:30 -0500

    added scrolling background

---
+++ Dolphin Rampage/Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	
+	public float speed=100f;
+	private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start () {
+		newPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newPosition.x += Time.deltaTime * speed;
+		transform.position = newPosition;
+	}
+}
---
+++ Dolphin Rampage/Assets/ScrollingScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingScript : MonoBehaviour {
+	private Transform cameraTransform;
+	private float spriteWidth;
+	// Use this for initialization
+	void Start () {
+		cameraTransform = Camera.main.transform;
+		SpriteRenderer spriteRenderer = GetComponent<Renderer>() as SpriteRenderer;
+		spriteWidth = spriteRenderer.sprite.bounds.size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((transform.position.x + spriteWidth) < cameraTransform.position.x) {
+			Debug.Log (transform.position.x + " + " + spriteWidth + "+" + cameraTransform.position.x);
+			Vector3 pos = transform.position;
+			pos.x += 2.0f * spriteWidth;
+			transform.position = pos;
+			Debug.Log (pos.ToString ());
+		}
+	}
+}
