4fcd1bb0d164ec9b6a7d53a349dcdfe8fb7782f6
Zparis42 <zgparis@mtu.edu>
2016-01-31 12:21:49 -0500

    Fixing the commit

---
+++ Dolphin Rampage/Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public double playerSpeed;
+	public Rigidbody2D body;
+	//public BoxCollider2D water;
+	//private GameObject water = GameObject.Find ("Water");
+	//public bool inWater;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		double horizontal = Input.GetAxis ("Horizontal") * playerSpeed;
+		double vertical = Input.GetAxis ("Vertical") * playerSpeed;
+		body.AddForce (new Vector2 ((float)horizontal, (float)vertical));
+
+		//if (inWater) {
+		//	body.gravityScale = .1f;
+		//} else {
+		//	body.gravityScale = 1f;
+		//}
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		body.gravityScale = .5f;
+
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		body.gravityScale = 1f;
+	}
+
+}
---
+++ Dolphin Rampage/Assets/Scripts/Water_Trigger.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water_Trigger : MonoBehaviour {
+
+	public Collider2D ThisTrigger;
+	public GameObject player;
+	public Collider2D playerCollider;
+	public bool characterInWater;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//	if (ThisTrigger.IsTouching (playerCollider)) {
+	//		playerCollider.attachedRigidbody.gravityScale = .1f;	
+	//	} else {
+	//		playerCollider.attachedRigidbody.gravityScale = 1f;
+	//	}
+	}
+
+	//void OnTriggerEnter2D(Collider2D other) {
+	//	characterInWater = true;
+	//}
+
+	//void OnTriggerExit2D(Collider2D other) {
+	//	characterInWater = false;
+	//}
+}
