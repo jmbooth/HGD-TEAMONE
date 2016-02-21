5bd159c79e23039a48755d7f66c53da0f7e994c1
Zparis42 <zgparis@mtu.edu>
2016-02-21 15:04:28 -0500

    Added harpooner and killbox

---
+++ Dolphin Rampage/Assets/Scripts/FireHarpoons.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireHarpoons : MonoBehaviour {
+
+	public GameObject harpoon;
+	private float timecount;
+
+	// Use this for initialization
+	void Start () {
+		timecount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timecount += 1;
+		if (timecount >= 120) {
+			Instantiate (harpoon, transform.position, Quaternion.identity);
+			timecount = 0;
+		}
+	}
+}
---
+++ Dolphin Rampage/Assets/Scripts/KillBox.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBox : MonoBehaviour {
+
+	public GameObject camera;
+	private Transform cameraPos;
+	private float offset;
+
+	// Use this for initialization
+	void Start () {
+		cameraPos = camera.transform;
+		offset = cameraPos.position.x - transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (cameraPos.position.x - offset, transform.position.y, transform.position.z);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Destroy (other.gameObject);
+	}
+}
