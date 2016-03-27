639bfa8631f35894961ae4f527e0623c99fdbb69
Zachary Paris <zgparis@mtu.edu>
2016-03-27 14:56:50 -0400

    Added a mine prefab

---
+++ Dolphin Rampage/Assets/Scripts/MineFloat.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MineFloat : MonoBehaviour {
+
+	private Vector3 basePosition;
+	private float floatPos;
+	public float floatRange;
+	private bool up;
+	public float floatRate;
+
+	// Use this for initialization
+	void Start () {
+		basePosition = transform.position;
+		floatPos = 0;
+		up = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (up) {
+			if (floatPos <= floatRange) {
+				floatPos += (Math.Min((floatRange - floatPos), (floatRange + floatPos)) / 64f) + .005f;
+			} else {
+				up = false;
+			}
+		} else {
+			if (floatPos >= -floatRange) {
+				floatPos -= (Math.Min((floatRange - floatPos), (floatRange + floatPos)) / 64f) + .005f;
+			} else {
+				up = true;
+			}
+		}
+
+		transform.position = new Vector3 (basePosition.x, basePosition.y + floatPos, basePosition.z);
+	}
+}
