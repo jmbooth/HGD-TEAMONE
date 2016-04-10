59f3b1c992b4c48eec827d6cfc82fe54c340c04d
Micah Kempf <mdkempf@mtu.edu>
2016-04-10 13:25:17 -0400

    Fisherman and boats now sink when their boat is destroyed. Harpoons now kill other harpooners and fisherman.  Boat destruction moves the boat into the killbox instead of destroying it immediately.  Bottom edge collider added to stop objects from sinking off the screen.

--- Dolphin Rampage/Assets/Scripts/FireHarpoons.cs
+++ Dolphin Rampage/Assets/Scripts/FireHarpoons.cs
@@ -16,9 +16,13 @@ public class FireHarpoons : MonoBehaviour {
 	void Update () {
 		timecount += 1;
 		if (timecount >= 120) {
+
 			Vector3 temp = transform.position;
 			temp += new Vector3 (-1.2f, -.28f, 0f);
 			Instantiate (harpoon, temp, boob.transform.rotation);
+
+			//Instantiate (harpoon, new Vector3(transform.position.x-3f,transform.position.y), Quaternion.identity);
+
 			timecount = 0;
 		}
 	}
