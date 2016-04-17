f622838e2edd0dce52a7fb17d947ed622d5daeb6
Johnathen <jmbooth@mtu.edu>
2016-04-17 14:34:24 -0400

    adding music scripts

---
+++ Dolphin Rampage/Assets/Scripts/FadeInMusic.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInMusic : MonoBehaviour {
+	
+	[TooltipAttribute("The audio source")]
+	public AudioSource audioSource;
+	[TooltipAttribute("Time in seconds to fade out")]
+	public float fadeSpeed = 1f;
+
+	private bool fadingIn = true;
+	
+	float time = 0f;
+	
+	void Update(){
+
+		if (fadingIn) {
+			audioSource.volume = Mathf.Lerp (0f, 1f, time);
+			time += Time.deltaTime / fadeSpeed;
+			Debug.Log (audioSource.volume);
+		}
+		if(audioSource.volume >= 1)
+			fadingIn = false;
+
+	}
+}
---
+++ Dolphin Rampage/Assets/Scripts/FadeoutMusic.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeoutMusic : MonoBehaviour {
+
+	[TooltipAttribute("The audio source")]
+	public AudioSource audioSource;
+	[TooltipAttribute("Time in seconds to fade out")]
+	public float fadeSpeed = 1f;
+	[TooltipAttribute("Toggle the fade")]
+	public bool startFadeIn = false;
+	public bool startFadeOut = false;
+
+	float time = 0f;
+	
+	void Update(){
+		if(startFadeOut){
+			audioSource.volume = Mathf.Lerp(1f, 0f, time);
+			time += Time.deltaTime / fadeSpeed;
+		}
+
+		if (audioSource.volume <= 0f)
+			startFadeOut = false;
+
+		if (startFadeIn) {
+			audioSource.volume = Mathf.Lerp(0f, 1f, time);
+			time += Time.deltaTime / fadeSpeed;
+		}
+
+		if (audioSource.volume >= 1f)
+			startFadeIn = false;
+	}
+}
